1c289d5bf75d4a278dbdac61a942c3cc5df951d4
verymysteriousacc <ihsanodabas29@gmail.com>
2026-05-17 04:12:42 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const fetch = require("node-fetch");
+
+const app = express();
+app.use(express.json());
+
+app.post("/geo", async (req, res) => {
+    const q = req.body.q;
+
+    if (!q) return res.status(400).json({ error: "missing q" });
+
+    const url = `https://nominatim.openstreetmap.org/search?q=${encodeURIComponent(q)}&format=json&limit=5`;
+
+    const response = await fetch(url, {
+        headers: {
+            "User-Agent": "RobloxOSMProject"
+        }
+    });
+
+    const data = await response.json();
+
+    res.json(data.map(p => ({
+        name: p.display_name,
+        lat: Number(p.lat),
+        lon: Number(p.lon)
+    })));
+});
+
+app.listen(process.env.PORT || 3000);
